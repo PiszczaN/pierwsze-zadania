7771bb969b05e6f66142ca09879bc45b2918298b
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-12 12:05:09 +0200

    method that generates the email address

--- generateHuman/script.js
+++ generateHuman/script.js
@@ -1,26 +1,61 @@
+//import { v4 as uuidv4 } from 'uuid';
 'use strict';
 
+const arrayOfPeople = [];
+
+/////////////////////////////////////
+//////// MAIN FUNCTION //////////////
+/////////////////////////////////////
 
 const generateHuman = function () {
 
     const human = {
-        name: "test",
-        surname: "test",
-        email: "test",
+
+        name: "name",
+        surname: "surname",
         age: generateRandomNumber(18, 85),
-        phoneNumber: 123,
+        phoneNumber: generateRandomNumber(100000000, 999999999),
         country: "test",
-        id: 1
+        id: generateRandomNumber(1, 99),
+
+        email: function () {
+            //the method after the first execution will overwrite its field with an automatically generated email address
+            this.email = this.name + this.surname + "@test.pl";
+        }
     }
 
+    //execute method that generates an email address
+    human.email();
+
+    arrayOfPeople.push(human);
 
     return human;
 }
+
+/////////////////////////////////////
+////////// FUNCTIONS ////////////////
+/////////////////////////////////////
 
 const generateRandomNumber = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+
+/////////////////////////////////////
+//////////// TESTS //////////////////
+/////////////////////////////////////
+
+// checking if the generateHuman() will add new objects to the arrayOfPeople[]
+
+generateHuman();
+generateHuman();
+generateHuman();
+console.log(arrayOfPeople);
+
 
 console.log(generateHuman());
