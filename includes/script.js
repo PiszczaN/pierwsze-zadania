b7cc700a9456e919ab01c0ceb0526c77d28f8cea
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-16 01:54:26 +0200

    functionality for strings

--- includes/script.js
+++ includes/script.js
@@ -3,6 +3,7 @@
 function includes(array, elementToFind, fromIndex = 0) {
     const arrayLength = array.length;
     let startFrom = Math.floor(fromIndex);
+    let j = 0;
 
     if (arrayLength === 0)
         return false;
@@ -17,9 +18,15 @@ function includes(array, elementToFind, fromIndex = 0) {
         if (typeof (array) === typeof (['array']) && (currentElement === elementToFind || (currentElement !== currentElement && elementToFind !== elementToFind)))
             return true;
 
-        if (typeof (array) === typeof ('string')) {
+        if (typeof (array) === typeof ('string') && typeof (elementToFind) === typeof ('string')) {
 
+            if (currentElement === elementToFind[j] && j + 1 <= elementToFind.length)
+                j++;
+            else
+                j = 0;
 
+            if (j === elementToFind.length)
+                return true;
 
         }
 
@@ -28,18 +35,9 @@ function includes(array, elementToFind, fromIndex = 0) {
     return false;
 }
 
-const includesInArray = function () {
-
-}
-
-const includesInString = function () {
-
-}
-
 const tab = [1, 2, 3, 'world', 6, NaN, 3];
 const sent = 'hello world'
 const word = 'world';
-console.log(tab.includes(word, -30));
 
-
-console.log(includes(tab, NaN, -1));
+console.log(sent.includes('hello world'));
+console.log(includes(sent, word));
