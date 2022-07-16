6448a9de4c22c1140f65cb427c66f10fb5daf9e5
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-17 01:02:57 +0200

    includes: added comments

--- includes/script.js
+++ includes/script.js
@@ -1,31 +1,51 @@
 'use strict';
 
+/////////////////////////////////////
+//////// MAIN FUNCTION //////////////
+/////////////////////////////////////
+
 function includes(array, elementToFind, fromIndex = 0) {
+
+    // saving 'fromIndex' in specific variable which rounds down a possible non-integer number
+    // the variable 'stringChecker' is used to diagnose whether the searched STRING is in the STRING passed as the place from which we are looking
     const arrayLength = array.length;
     let startFrom = Math.floor(fromIndex);
-    let j = 0;
+    let stringChecker = 0;
+
+    // if 'array' is empty return false
 
     if (arrayLength === 0)
         return false;
+
+    //if fromIndex has negative value the function searches the array from ([arrayLength] + [fromIndex]) to the end
+
+    if (startFrom < 0)
+        startFrom = arrayLength + startFrom;
+
+    //iterating through array or string and holding current element in 'currentElement'
 
     for (let i = startFrom; i < arrayLength; ++i) {
 
         const currentElement = array[i];
 
-        if (i < 0)
-            i = arrayLength + i - 1;
+        // IF the function gets an ARRAY to be searched
+        // if current element is equal to element to find function will return true
+        // OR if element to find is NaN - checking if currentElement !== currentElement && elementToFind !== elementToFind
+        // BECAUSE NaN !== NaN
 
         if (typeof (array) === typeof (['array']) && (currentElement === elementToFind || (currentElement !== currentElement && elementToFind !== elementToFind)))
             return true;
 
+        //IF the function gets an STRING to be searched
+
         if (typeof (array) === typeof ('string') && typeof (elementToFind) === typeof ('string')) {
 
-            if (currentElement === elementToFind[j] && j + 1 <= elementToFind.length)
-                j++;
+            if (currentElement === elementToFind[stringChecker])
+                stringChecker++;
             else
-                j = 0;
+                stringChecker = 0;
 
-            if (j === elementToFind.length)
+            if (stringChecker === elementToFind.length)
                 return true;
 
         }
@@ -35,9 +55,14 @@ function includes(array, elementToFind, fromIndex = 0) {
     return false;
 }
 
+
+/////////////////////////////////////
+//////////// TESTS //////////////////
+/////////////////////////////////////
+
 const tab = [1, 2, 3, 'world', 6, NaN, 3];
 const sent = 'hello world'
 const word = 'world';
 
-console.log(sent.includes('w', 7));
-console.log(includes(sent, 'w', 7));
+console.log(sent.includes(' '));
+console.log(includes(sent, ' '));
