8c93454040ecf871e7cecc7b56f081ca1d034ac9
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-19 20:48:41 +0200

    ArrayMethods: forEachFn

--- arrayMethods/script.js
+++ arrayMethods/script.js
@@ -1,4 +1,10 @@
-const forEachFn = (array, callback) => { };
+const forEachFn = (array, callback) => {
+
+    for (const [iterator, value] of array.entries()) {
+        callback(value, iterator);
+    }
+
+};
 
 const mapFn = (array, callback) => { };
 
@@ -11,3 +17,24 @@ const reduceFn = (array, callback, inital) => { };
 const everyFn = (array, callback) => { };
 
 const someFn = (array, callback) => { };
+
+
+
+
+/////////////////////////////////////
+//////////// TESTS //////////////////
+/////////////////////////////////////
+
+const array = [1, 2, 'egse', 4, 5, 6];
+
+forEachFn([1, 2, 'egse', 4, 5, 6], (element, index) => {
+    element = element + 2;
+    console.log(`Testing forEachFn: at index: ${index} there is element: ${element}`);
+})
+
+console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+array.forEach((element, index) => {
+    element = element + 2;
+    console.log(`Testing .forEach: at index: ${index} there is element: ${element}`);
+})
