3f10bb7915042563b4e461e597402d3e298398be
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-19 19:13:22 +0200

    ArrayMethods: init

---
+++ arrayMethods/script.js
@@ -0,0 +1,13 @@
+const forEachFn = (array, callback) => { };
+
+const mapFn = (array, callback) => { };
+
+const entriesFn = (array) => { };
+
+const filterFn = (array, callback) => { };
+
+const reduceFn = (array, callback, inital) => { };
+
+const everyFn = (array, callback) => { };
+
+const someFn = (array, callback) => { };
