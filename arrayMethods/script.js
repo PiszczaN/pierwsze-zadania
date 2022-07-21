11cce695259355227b99842082fb4d30dd24f546
Jakub Piszczan <jpiszczan@gmail.com>
2022-07-22 00:01:48 +0200

    ArrayMethods: filter

--- arrayMethods/script.js
+++ arrayMethods/script.js
@@ -41,6 +41,18 @@ const filterFn = (array, callback) => {
         throw Error(`TypeError: ${array}.forEach is not a function`);
 
     const arrayCopy = array.slice(0);
+    let resultArray = [];
+
+    for (let index = 0; index < arrayCopy.length; ++index) {
+        const element = arrayCopy[index];
+
+        const resultElement = callback(element, index, arrayCopy);
+
+        if (resultElement)
+            resultArray.push(element);
+    }
+
+    return resultArray;
 };
 
 const reduceFn = (array, callback, inital) => {
@@ -72,6 +84,28 @@ const someFn = (array, callback) => {
 
 const array = [1, 2, 'egse', 4, 5, 6];
 
+// MAP
+
+const testFilterFn = filterFn(array, element => element > 3);
+console.log(testFilterFn);
+
+console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+const testFilter = array.filter(element => element > 3);
+console.log(testFilter);
+
+// MAP
+/*
+const testMapFn = mapFn(array, element => element = element + 2);
+console.log(testMapFn);
+
+console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+const testMap = array.map(element => element = element + 2);
+console.log(testMap);
+*/
+// FOREACH
+/*
 forEachFn(array, (element, index) => {
     element = element + 2;
     console.log(`Testing forEachFn: at index: ${index} there is element: ${element}`);
@@ -83,3 +117,4 @@ array.forEach((element, index) => {
     element = element + 2;
     console.log(`Testing .forEach: at index: ${index} there is element: ${element}`);
 })
+*/
