e6548a0120d9103ca8cc51d941f63dda02d6fe57
Jakub Piszczan <jpiszczan@gmail.com>
2022-08-01 20:30:26 +0200

    ArrayMethods: general improvements + fixed reduce

--- arrayMethods/script.js
+++ arrayMethods/script.js
@@ -5,10 +5,14 @@
 const forEachFn = (array, callback) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.forEachFn is not a function`);
 
-    for (const [iterator, value] of array.entries()) {
-        callback(value, iterator);
+    const arrayCopy = array;
+
+    for (let index = 0; arrayCopy.length; ++index) {
+        const element = arrayCopy[index];
+
+        callback(element, index, arrayCopy);
     }
 
 };
@@ -20,7 +24,7 @@ const forEachFn = (array, callback) => {
 const mapFn = (array, callback) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.mapFn is not a function`);
 
     const arrayCopy = array.slice(0);
     let resultArray = [];
@@ -43,13 +47,15 @@ const mapFn = (array, callback) => {
 const entriesFn = (array) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.entriesFn is not a function`);
 
     const arrayCopy = array.slice(0);
     let resultArray = [];
 
     for (let index = 0; index < arrayCopy.length; ++index) {
-        const entry = [index, arrayCopy[index]];
+        const element = arrayCopy[index];
+
+        const entry = [index, element];
         resultArray.push(entry);
     }
 
@@ -64,13 +70,16 @@ const entriesFn = (array) => {
 const filterFn = (array, callback) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.filterFn is not a function`);
 
     const arrayCopy = array.slice(0);
     let resultArray = [];
 
     for (let index = 0; index < arrayCopy.length; ++index) {
         const element = arrayCopy[index];
+
+        if (element === undefined)
+            continue;
 
         const resultElement = callback(element, index, arrayCopy);
 
@@ -85,20 +94,36 @@ const filterFn = (array, callback) => {
 //////////// REDUCE /////////////////
 /////////////////////////////////////
 
-const reduceFn = (array, callback, initial = 0) => {
+const reduceFn = function (array, callback, initial) {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.reduceFn is not a function`);
+
+    if (array.length === 0 && arguments.length !== 3) {
+        throw Error(`Reduce of empty array with no initial value`);
+    }
+
+    //  INITIAL is a value to which ACCUMULATOR is initialized the first time the callback is called.
+    //  If INITIAL is specified, that also causes CURRENTVALUE to be initialized to the first value in the array.
+    //  If INITIAL is not specified, ACCUMULATOR is initialized to the first value in the array,
+    //  and CURRENTVALUE is initialized to the second value in the array.
 
     const arrayCopy = array.slice(0);
-    let accumulator = initial;
+    let accumulator = arrayCopy[0];
+    let index = 1;
 
-    for (const [index, value] of entriesFn(arrayCopy)) {
+    if (arguments.length === 3) {
+        accumulator = initial;
+        index = 0;
+    }
 
-        if (value === undefined)
+    for (index; index < arrayCopy.length; ++index) {
+        const currentValue = arrayCopy[index];
+
+        if (currentValue === undefined)
             continue;
 
-        accumulator = callback(accumulator, value, index, arrayCopy);
+        accumulator = callback.call(accumulator, accumulator, currentValue, index, arrayCopy);
     }
 
     return accumulator;
@@ -111,7 +136,7 @@ const reduceFn = (array, callback, initial = 0) => {
 const everyFn = (array, callback) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.everyFn is not a function`);
 };
 
 /////////////////////////////////////
@@ -121,7 +146,7 @@ const everyFn = (array, callback) => {
 const someFn = (array, callback) => {
 
     if (!Array.isArray(array))
-        throw Error(`TypeError: ${array}.forEach is not a function`);
+        throw Error(`TypeError: ${array}.someFn is not a function`);
 };
 
 
@@ -135,22 +160,42 @@ const someFn = (array, callback) => {
 //////////// TESTS //////////////////
 /////////////////////////////////////
 
-const array = [1, 2, 'egse', , 5, 6, null];
+const array = [1, 2, 'egse', , 5, 6, , null, NaN];
+const array1 = [1];
+const arrayUn = [undefined]
+const array0 = [];
+//console.log(`TEST: ${array[3]} ( ${typeof (array[3])} ) vs ${array[4]} ( ${typeof (array[4])} ) is:  ${array[3] === array[4]}`);
+//console.log(`INDEX OF UNDEFINED : ${array.indexOf(undefined)}`);
+
 
 // REDUCE
+/*
+const reduceFnTest = reduceFn(array1, (acc, element, index) => {
+    //console.log(this);
 
-const reduceFnTest = reduceFn(array, (acc, element, index) => {
     return acc + element;
-})
+}, 2)
+
+//const reduceFnTest2 = reduceFn(array1, function (acc, element, index) {
+//    console.log(this);
+//
+//   return acc + element;
+//}, 2)
 console.log(reduceFnTest);
 
 console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
 
-const reduceTest = array.reduce((acc, element, index) => {
+const reduceTest = array1.reduce((acc, element, index) => {
+    //console.log(this);
     return acc + element;
+}, 2)
 
-})
+//const reduceTest2 = array1.reduce(function (acc, element, index) {
+//    console.log(this);
+//    return acc + element;
+//}, 2)
 console.log(reduceTest);
+*/
 // ENTRIES
 /*
 for (const [iterator, value] of entriesFn(array)) {
@@ -163,14 +208,20 @@ for (const [iterator, value] of array.entries()) {
     console.log(`.entries(): iterator: ${iterator} value: ${value}`);
 }
 */
-// MAP
+// FILTER
 /*
-const testFilterFn = filterFn(array, element => element > 3);
+const testFilterFn = filterFn(array, (element, index) => {
+    console.log(`index: ${index}, element: ${element}`);
+    return element;
+});
 console.log(testFilterFn);
 
 console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
 
-const testFilter = array.filter(element => element > 3);
+const testFilter = array.filter((element, index) => {
+    console.log(`index: ${index}, element: ${element}`);
+    return element;
+});
 console.log(testFilter);
 */
 // MAP
